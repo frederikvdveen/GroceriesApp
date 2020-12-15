d2abc07d0ce59c9b7759756473881979f840716a
Frederik van der Veen <frederik.v.d.veen@icloud.com>
2020-12-15 15:42:29 +0100

    test commit

--- Einkaufsliste/ContentView.swift
+++ Einkaufsliste/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var usedWords = [String]()
+    @State var rootWord = ""
+    @State var newWord = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            TextField("Neuer Eintrag", text: $newWord, onCommit: addNewWord)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            List{
+                ForEach(usedWords) { word in
+                    Text(usedWords)
+                }
+            }
+        }
+    }
+    func addNewWord() {
+        let answer =
+            newWord.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            return
+        }
+        // extra valiation
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+        
     }
 }
 
