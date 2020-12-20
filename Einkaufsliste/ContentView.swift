ebfb4bf9f4b522b181209accdc355db5b6858425
Jan van der Veen <j.van.der.veen@me.com>
2020-12-20 18:55:43 +0100

    Suggestions added

--- Einkaufsliste/ContentView.swift
+++ Einkaufsliste/ContentView.swift
@@ -12,31 +12,40 @@ struct ContentView: View {
     @State var GroceriesArray = [GrocerieItem]()
     @State var newEntry = ""
     
+    @State var typing = false
+
     var body: some View {
         VStack{
-            TextField("Neuer Eintrag", text: $newEntry, onCommit: addNewEntry)
+            
+            TextField("Neuer Eintrag", text: $newEntry, onEditingChanged: { self.typing = $0 }, onCommit: addNewEntry)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
-           
-            List{
-                ForEach(GroceriesArray, id: \.self){ data in
-                    HStack{
-                        Text(data.name)
-                        Spacer()
-                        Text("\(data.quantity)")
-                            .fontWeight(.thin)
-//                            .padding()
-                        Text("\(data.unit)")
-                            .fontWeight(.thin)
-                    }
-                    
-                }
-                .onDelete(perform: deleteEntry)
-            }
             
+            if typing && !newEntry.isEmpty {
+                List {
+                    ForEach(products.filter { $0.hasPrefix(newEntry)}, id: \.self) { data in
+                        Text(data)
+                    }
+                }
+            } else {
+                List {
+                    ForEach(GroceriesArray, id: \.self){ data in
+                        HStack{
+                            Text(data.name)
+                            Spacer()
+                            Text("\(data.quantity)")
+                                .fontWeight(.thin)
+                            Text("\(data.unit)")
+                                .fontWeight(.thin)
+                        }
+                        
+                    }
+                    .onDelete(perform: deleteEntry)
+                }
+            }
         }
     }
-    
+
     func addNewEntry() {
         let answer =
             GrocerieItem(name: newEntry.trimmingCharacters(in: .whitespacesAndNewlines), quantity: 2.7, unit: "kg")
